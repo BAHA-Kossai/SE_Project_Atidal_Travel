235ae1457c738873d161213c1765745f48e7f0d2
BAHA Kossai <kossai.baha@ensia.edu.dz>
2025-11-16 12:18:25 +0100

    creating local main structure

---
+++ client/src/Pages/admin_dashboard/admin_dashboard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+/**
+ * File: admin_dashboard.jsx
+ * Description: Handles the admin dashboard UI functionality.
+ * 
+ * Created By: Kossai Baha
+ * Created On: 16-Nov-2025
+ * Version: 1.0.0
+ * Last Modified By: Kossai Baha
+ * Last Modified On: 16-Nov-2025
+ * 
+ * Notes:
+ * - Initial creation of admin dashboard interface.
+ * - Includes user management and analytics widgets.
+ */
+
+
+// AdminDashboard main component
+export const AdminDashboard = ()=>{
+    return <h1>test</h1>;
+}
