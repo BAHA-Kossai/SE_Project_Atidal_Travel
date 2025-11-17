6b5916743d727627a72852e55597bbf18c90bc78
Youssef Touahria Miliani <youssef.touahria.miliani@ensia.edu.dz>
2025-11-17 16:30:10 +0100

    Update TabSecurity.jsx

--- client/src/components/InputField.jsx
+++ client/src/components/InputField.jsx
@@ -1,8 +1,13 @@
-export default function InputField({label, value, type = "text"}) {
+export default function InputField({label, value, type = "text", fieldMaxWidth = null}) {
     return (
-        <main className="flex flex-col text-gray-700">
+        <main className={`flex flex-col text-gray-700`}>
             <label className={"mb-2 font-semibold"}>{label}</label>
-            <input type={type} disabled={true} value={value} className={"border-1 py-2 px-3 border-gray-500 rounded-lg"}/>
+            <input
+                type={type}
+                disabled={true}
+                value={value}
+                className={`border-1 py-3 px-3 border-gray-500 rounded-lg max-w-${fieldMaxWidth}`}
+            />
         </main>
     )
 }
--- client/src/components/admin-settings/security/TabSecurity.jsx
+++ client/src/components/admin-settings/security/TabSecurity.jsx
@@ -5,20 +5,41 @@ import Setting from "../Setting.jsx";
 import Switch from "../../Switch.jsx";
 
 export default function TabSecurity() {
+    const fields = [
+        {
+            label: "Email",
+            type: "text",
+            value: "email@gmail.com",
+            fieldMaxWidth: 100,
+            buttonText: "Change Email"
+        },
+        {
+            label: "Password",
+            type: "password",
+            value: "email@gmail.com",
+            fieldMaxWidth: 100,
+            buttonText: "Change Password"
+        }
+    ]
+
     return (
         <WhiteContainer title={"Security settings"}>
-            <div className={"flex flex-row justify-between items-end mb-5"}>
-                <InputField label={"Email"} value={"email@gmail.com"}/>
-                <ButtonOutline children={"Change Email"}/>
-            </div>
-            <div className={"flex flex-row justify-between items-end mb-5"}>
-                <InputField type={"password"} label={"Password"} value={"email@gmail.com"}/>
-                <ButtonOutline children={"Change Password"}/>
-            </div>
+            {
+                fields.map((field, ) => {
+                    return (
+                        <div className={"flex flex-row justify-between items-end mb-5"}>
+                            <InputField label={field.label} value={field.value} fieldMaxWidth={field.fieldMaxWidth}/>
+                            <ButtonOutline children={field.buttonText}/>
+                        </div>
+                    )
+                })
+            }
+
             <Setting
                 title={"2-Step Authentication"}
                 description={"add an additional layer of security to your account during login"}
-                button={<Switch/>}/>
+                button={<Switch/>}
+            />
         </WhiteContainer>
     )
 }
