cbdc1c1c25fa4c892fba90e562a8d865cdf721de
Youssef Touahria Miliani <youssef.touahria.miliani@ensia.edu.dz>
2025-11-19 22:52:42 +0100

    Revert merging from frontend into admin_dashboard
    This reverts commit 5ec71d0dd222ba7a6232afe0a8b47835fb173254, reversing
    changes made to 64c901ed527abce8f90a60c20d1024c8366a0ea2.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import "./styles/admin_dashboard/admin_dashboard.css";
 import { Camera, Plane ,TrendingUp,} from "lucide-react";
 import { DecoratedCard, StatusCard } from './Pages/admin_dashboard/components/status_cards';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {AdminDashboard} from './Pages/admin_dashboard/admin_dashboard';
-import AdminSettings from './pages/admin_settings';
-import "./styles/admin_dashboard/admin_dashboard.css";
 
+import {AdminDashboard} from './Pages/admin_dashboard/admin_dashboard';
 
 
 const rootElement = document.getElementById('root') ;
 
 createRoot(rootElement).render(
-
-    <BrowserRouter>
-        <Routes>
-            <Route path="/admin-settings" element={<AdminSettings />} />
-            <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        </Routes>
-    </BrowserRouter>
+   <AdminDashboard />
 
 
 );
