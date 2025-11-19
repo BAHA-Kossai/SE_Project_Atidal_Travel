b6d0856ab5650780e40896e2ab59cb84ed6e82d2
Youssef Touahria Miliani <youssef.touahria.miliani@ensia.edu.dz>
2025-11-19 23:04:19 +0100

    Minor fixes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import AdminSettings from './pages/admin_settings';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Camera, Plane ,TrendingUp,} from "lucide-react";
-import { DecoratedCard, StatusCard } from './Pages/admin_dashboard/components/status_cards';
 import {AdminDashboard} from './Pages/admin_dashboard/admin_dashboard';
 import "./styles/admin_dashboard/admin_dashboard.css";
 
 const rootElement = document.getElementById('root');
 
 createRoot(rootElement).render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/admin-settings" element={<AdminSettings />} />
-            <Route path="/admin-settings" element={<AdminDashboard />} />
-        </Routes>
-    </BrowserRouter>
+    <AdminDashboard/>
 );
