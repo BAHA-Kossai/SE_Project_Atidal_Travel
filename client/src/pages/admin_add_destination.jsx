845d03b831610b1c36dd8b31c8b22dd548267841
Youssef Touahria Miliani <youssef.touahria.miliani@ensia.edu.dz>
2025-11-20 16:25:38 +0100

    Update admin_add_destination.jsx

--- client/src/pages/admin_add_destination.jsx
+++ client/src/pages/admin_add_destination.jsx
@@ -1,6 +1,10 @@
 import {AppBarSideBarWithContent} from "../components/appBarSideBar.jsx";
 import WhiteContainer from "../components/WhiteContainer.jsx";
 import PagePath from "../components/PagePath.jsx";
+import InputField from "../components/InputField.jsx";
+import {UploadCloud, UploadIcon} from "lucide-react";
+import ButtonOutline from "../components/ButtonOutline.jsx";
+import ButtonFill from "../components/ButtonFill.jsx";
 
 export default function AdminAddDestination() {
 
@@ -15,7 +19,45 @@ export default function AdminAddDestination() {
                 <PagePath pathItems={["Dashboard", "Destinations", "Add a destination"]}/>
             </div>
 
-            <WhiteContainer title={"Add a destination"}>
+            <WhiteContainer title={"Add new destination"}>
+
+                <div className={"flex flex-row justify-around"}>
+
+                    {/* Upload Image */}
+                    <div className="border-4 border-dashed border-(--color-primary) rounded-2xl cursor-pointer"
+                         style={{
+                             backgroundColor: "#f4f8fb",
+                             borderColor: "#669bbc",
+                         }}
+                    >
+                        <div className={"flex flex-col items-center p-8"}>
+                            <UploadCloud size={140} className={"text-(--color-primary)"}
+                                         style={{
+                                             color: "#669bbc",
+                                         }}/>
+                            <div className={"flex flex-row items-center"}>
+                                <UploadIcon className={"mr-2"}
+                                            style={{
+                                                color: "#669bbc",
+                                            }}
+                                />
+                                Drop your files, or Browse
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            <InputField label={"Destination Country"} fieldMaxWidth={200}/>
+                            <InputField label={"Destination City(ies)"}/>
+                            <InputField label={"First name"}/>
+                        </div>
+                        <div className={"flex flex-row"}>
+                            <ButtonOutline>Cancel</ButtonOutline>
+                            <ButtonFill>Add destination</ButtonFill>
+                        </div>
+                    </div>
+                </div>
             </WhiteContainer>
         </AppBarSideBarWithContent>
     )
