ff4c2bdc18c0411c749699b8ace24d8fca15c4e5
Youssef Touahria Miliani <youssef.touahria.miliani@ensia.edu.dz>
2025-11-23 16:45:19 +0100

    Remove Change Appearance Preference

--- client/src/components/admin-settings/ChangePreferences.jsx
+++ client/src/components/admin-settings/ChangePreferences.jsx
@@ -5,7 +5,7 @@ import Setting from "./Setting.jsx";
 export default function ChangePreferences() {
     return (
         // Container
-        <main className="h-43 flex flex-col justify-between relative bg-white font-normal mt-5 bg-whie border-2 border-gray-200 p-5 rounded-3xl">
+        <main className="relative bg-white font-normal mt-5 bg-whie border-2 border-gray-200 p-5 rounded-3xl">
             {/* Preferences */}
             <Setting
                 title={"Change Language"}
@@ -13,16 +13,6 @@ export default function ChangePreferences() {
                 button={
                     <ButtonOutline width={50}>
                         English
-                        <ChevronDown size={20} className="ml-2"/>
-                    </ButtonOutline>
-                }
-            />
-            <Setting
-                title={"Change Appearance"}
-                description={"you have the choice between light and dark mode"}
-                button={
-                    <ButtonOutline width={50}>
-                        Light
                         <ChevronDown size={20} className="ml-2"/>
                     </ButtonOutline>
                 }
--- client/src/components/admin-settings/Setting.jsx
+++ client/src/components/admin-settings/Setting.jsx
@@ -1,7 +1,7 @@
 export default function Setting({title, description, button}) {
     return (
         <>
-            <div className={"flex flex-row justify-between items-center mb-4"}>
+            <div className={"flex flex-row justify-between items-center"}>
                 <div className="ml-3">
                     <div className={"text-[20px] font-normal"}>{title}</div>
                     <div className={"text-[15px] text-gray-500"}>{description}</div>
