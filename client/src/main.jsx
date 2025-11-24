f2bdf4aacb76b43c6fb53a4c5c6cb5f6895b59d0
Youssef Touahria Miliani <youssef.touahria.miliani@ensia.edu.dz>
2025-11-24 19:01:38 +0100

    Minor fixes after merging

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AdminSettings from './pages/admin_settings';
+import AdminSettings from './pages/admin_settings.jsx';
 import AdminDestinations from "./pages/admin_destinations.jsx";
 import AdminEmployees from "./pages/admin_employees.jsx";
+import {AdminDashboard} from "./pages/admin_dashboard/admin_dashboard.jsx";
 
 
 const rootElement = document.getElementById('root');
@@ -10,6 +11,7 @@ const rootElement = document.getElementById('root');
 createRoot(rootElement).render(
     <BrowserRouter>
         <Routes>
+            <Route path="/admin-dashboard" element={<AdminDashboard/>} />
             <Route path="/admin-settings" element={<AdminSettings/>} />
             <Route path="/admin-destinations" element={<AdminDestinations/>} />
             <Route path="/admin-employees" element={<AdminEmployees/>} />
