5772c71ec52a762f54d53355f8c13563e89a7a06
Youssef Touahria Miliani <youssef.touahria.miliani@ensia.edu.dz>
2025-11-25 18:31:23 +0100

    Add Malak's work to the frontend branch
    - Add the used files and folders to the frontend structure
    - Integrate the pages' navigation and routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,18 +3,33 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminSettings from './pages/admin_settings.jsx';
 import AdminDestinations from "./pages/admin_destinations.jsx";
 import AdminEmployees from "./pages/admin_employees.jsx";
-import {AdminDashboard} from "./pages/admin_dashboard/admin_dashboard.jsx";
+import { AdminDashboard } from "./pages/admin_dashboard/admin_dashboard.jsx";
+import BookingPage from "./Pages/BookingForm.jsx";
+import HomePage from "./Pages/Homepage.jsx";
+import UmrahPage from "./Pages/UmrahPage.jsx";
+import Destinations from "./Pages/Destinations.jsx";
+import NotFound from "./Pages/NotFound.jsx";
+import { StrictMode } from "react";
 
 
 const rootElement = document.getElementById('root');
 
 createRoot(rootElement).render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/admin-dashboard" element={<AdminDashboard/>} />
-            <Route path="/admin-settings" element={<AdminSettings/>} />
-            <Route path="/admin-destinations" element={<AdminDestinations/>} />
-            <Route path="/admin-employees" element={<AdminEmployees/>} />
-        </Routes>
-    </BrowserRouter>
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                {/* User Side */}
+                <Route path="/" element={<HomePage/>} />
+                <Route path="/umrah" element={<UmrahPage/>} />
+                <Route path="/bookings" element={<BookingPage/>} />
+                <Route path="/destinations" element={<Destinations/>} />
+                <Route path="*" element={<NotFound/>} />
+                {/* Admin Side */}
+                <Route path="/admin-dashboard" element={<AdminDashboard/>} />
+                <Route path="/admin-settings" element={<AdminSettings/>} />
+                <Route path="/admin-destinations" element={<AdminDestinations/>} />
+                <Route path="/admin-employees" element={<AdminEmployees/>} />
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>
 );
