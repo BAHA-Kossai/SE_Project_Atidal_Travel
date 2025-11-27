0ac3af45f3b681d5b8565f8987de89d2fc5be6de
Ahlem Toubrinet <ahlem.toubrinet@ensia.edu.dz>
2025-11-27 04:58:14 +0100

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,22 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
-import HomePage from './pages/HomePage.jsx'
-import Destinations from './pages/Destinations.jsx'
-import BookingForm from './pages/BookingForm.jsx'
-import NotFound from './pages/NotFound.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { supabase } from './config/supabase.js'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/destinations" element={<Destinations />} />
-        <Route path="/booking" element={<BookingForm />} />
-        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+function App() {
+  console.log(supabase); 
+  return (
+    <div>
+      <h1>This is the home page</h1>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
