c5010ccf5013d835cf7e958bef18bf4daea53bd7
Abderahim-14 <abderahim.khefif@ensia.edu.dz>
2025-12-28 13:45:08 +0100

    history abderahim

--- .history/server/src/config/config_20251125224336.js
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-
-export default {
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  PORT: process.env.PORT || 3000,
-  SUPABASE_URL: https://vpmyaghsxubnckxaujwq.supabase.co,
-  SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
-};
